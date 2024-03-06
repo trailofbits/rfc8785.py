862c9d8547f516b01195d548765cd0262d798219
William Woodruff <william@trailofbits.com>
2024-03-06 14:19:20 -0500

    rfc8785: CI, docstring coverage (#2)
    * rfc8785: docstring coverage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * workflows: fix python versions
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * fill out coverage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- src/rfc8785/__init__.py
+++ src/rfc8785/__init__.py
@@ -4,11 +4,12 @@ The `rfc8785` APIs.
 
 __version__ = "0.0.1"
 
-from ._impl import CanonicalizationError, IntegerDomainError, dump, dumps
+from ._impl import CanonicalizationError, FloatDomainError, IntegerDomainError, dump, dumps
 
 __all__ = [
     "CanonicalizationError",
     "IntegerDomainError",
+    "FloatDomainError",
     "dump",
     "dumps",
 ]
